1a06acf1e0ff149a1213b4d4bbadf2c0227e486d
Chris Li <kittokevin@gmail.com>
2017-03-15 10:29:44 -0700

    Attempt for quick loading pages

--- src/main/java/org/ret/app/Application.java
+++ src/main/java/org/ret/app/Application.java
@@ -1,19 +1,12 @@
 package org.ret.app;
 
-import org.ret.core.config.DatabaseConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import springfox.documentation.builders.PathSelectors;
@@ -41,13 +34,6 @@ public class Application extends SpringBootServletInitializer{
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
-    }
-    
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
-        factory.setPort(9000);
-        return factory;
     }
     
     @Bean
--- src/main/java/org/ret/core/config/WebConfig.java
+++ src/main/java/org/ret/core/config/WebConfig.java
@@ -1,22 +1,27 @@
-/*package org.ret.core.config;
+package org.ret.core.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.io.File;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+/*    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/static/",
+            "classpath:/static/js/", "classpath:/static/pages/",
+            "classpath:/static/images/", "classpath:/static/css/" };
+*/
     @Override
     public void addViewControllers(final ViewControllerRegistry registry) {
         registry.addViewController("/index.htm");
@@ -27,9 +32,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         configurer.enable();
     }
     
+/*    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }*/
+    
+    
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        //factory.setPort(8443);
+        return factory;
+    }
+    
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
-        
         return new EmbeddedServletContainerCustomizer() {
             @Override
             public void customize(ConfigurableEmbeddedServletContainer container) {
@@ -41,10 +58,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         };
     }
 
-
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
     }
 }
-*/
