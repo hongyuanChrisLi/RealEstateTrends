3b1c30aeda990a383d078215ae731f4c7d8b2785
Chris Li <kittokevin@gmail.com>
2017-03-15 20:32:27 -0700

    Chart display

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 
 // angular.js main app initialization
-var app = angular.module('RealEstateTrends', []).config(
+var app = angular.module('RealEstateTrends', ['ngRoute', 'chart.js']).config(
 		[ '$routeProvider', function($routeProvider) {
 			$routeProvider.when('/', {
 				templateUrl : 'pages/index.html',
@@ -29,3 +29,35 @@ var app = angular.module('RealEstateTrends', []).config(
 app.config([ '$locationProvider', function($location) {
 	$location.hashPrefix('!');
 } ]);
+
+app.controller("LineCtrl",function($scope) {
+
+			$scope.labels = [ "January", "February", "March", "April", "May",
+					"June", "July" ];
+			$scope.series = [ 'Series A', 'Series B' ];
+			$scope.data = [ [ 65, 59, 80, 81, 56, 55, 40 ],
+					[ 28, 48, 40, 19, 86, 27, 90 ] ];
+			$scope.onClick = function(points, evt) {
+				console.log(points, evt);
+			};
+			$scope.datasetOverride = [ {
+				yAxisID : 'y-axis-1'
+			}, {
+				yAxisID : 'y-axis-2'
+			} ];
+			$scope.options = {
+				scales : {
+					yAxes : [ {
+						id : 'y-axis-1',
+						type : 'linear',
+						display : true,
+						position : 'left'
+					}, {
+						id : 'y-axis-2',
+						type : 'linear',
+						display : true,
+						position : 'right'
+					} ]
+				}
+			};
+		});
