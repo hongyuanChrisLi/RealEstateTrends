51f0ec7e1d9ca9683c9e9e26857cc1a27b8c854d
Chris Li <kittokevin@gmail.com>
2017-03-17 17:03:55 -0700

    Share values between controllers

--- src/main/resources/static/js/ctrl-rest.js
+++ src/main/resources/static/js/ctrl-rest.js
@@ -2,18 +2,42 @@
 
 var app = angular.module("app");
 
+app.service('sharedProperties', function() {
+  var selectedCountyId = 0;
+  
+  return {
+    getSelectedCountyId: function(){
+      return selectedCountyId;
+    },
+    setSetlectedCountyId: function(value){
+      selectedCountyId = value;
+    }
+  };
+  
+});
+
+
 // Controller for County dropdown list
-app.controller('countyOptCtrl', function($scope, $http, $log){
+app.controller('countyOptCtrl', function($scope, $http, $log, sharedProperties){
   $http({
     method : 'GET',
     url : '/area/county'
   }).then(function successCallback(response){
-    var jsonCounties = response.data.responseData.counties;
-    var counties = [];
-    var num = jsonCounties.length;
-    for ( var i = 0; i < num; i++ ){
-      counties.push(jsonCounties[i].county)
+    $scope.counties = response.data.responseData.counties;
+    $scope.counties.unshift({'county': 'All', 'countyId': 0})
+    $scope.selectedCounty = $scope.counties[0]
+    $scope.onChange = function(){
+      $log.info($scope.selectedCounty)
+      $log.info($scope.selectedCounty['countyId'])
+      $log.info($scope.selectedCounty.countyId)
+      sharedProperties.setSetlectedCountyId($scope.selectedCounty.countyId);
     }
-    $scope.names = counties;
   })
 });
+
+app.controller('viewCtrl', function($scope, $log, sharedProperties){
+  $scope.countyId = sharedProperties.getSelectedCountyId();
+  $scope.onClick = function(){
+    $scope.countyId = sharedProperties.getSelectedCountyId();
+  }
+})
