f7307c272af906c5f5532f02a193f21889f7827e
Chris Li <kittokevin@gmail.com>
2017-03-17 17:18:58 -0700

    Add city dropdown list

--- src/main/resources/static/js/ctrl-rest.js
+++ src/main/resources/static/js/ctrl-rest.js
@@ -4,6 +4,7 @@ var app = angular.module("app");
 
 app.service('sharedProperties', function() {
   var selectedCountyId = 0;
+  var selectedCityId = 0;
   
   return {
     getSelectedCountyId: function(){
@@ -11,6 +12,12 @@ app.service('sharedProperties', function() {
     },
     setSetlectedCountyId: function(value){
       selectedCountyId = value;
+    },
+    getSelectedCityId: function(){
+      return selectedCityId;
+    },
+    setSelectedCityId: function(value){
+      selectedCityId = value;
     }
   };
   
@@ -27,14 +34,35 @@ app.controller('countyOptCtrl', function($scope, $http, $log, sharedProperties){
     $scope.counties.unshift({'county': 'All', 'countyId': 0})
     $scope.selectedCounty = $scope.counties[0]
     $scope.onChange = function(){
-      $log.info($scope.selectedCounty)
-      $log.info($scope.selectedCounty['countyId'])
-      $log.info($scope.selectedCounty.countyId)
       sharedProperties.setSetlectedCountyId($scope.selectedCounty.countyId);
     }
   })
 });
 
+
+// Controller for City dropdown list
+app.controller('cityOptCtrl', function($scope, $http, $log, sharedProperties){
+  $scope.selectedCityId = sharedProperties.getSelectedCityId();
+  $scope.cities = [{'city': 'All', 'cityId': 0}]
+  $scope.selectedCity = $scope.cities[$scope.selectedCityId];
+  $scope.onEnter = function(){
+    $scope.countyId = sharedProperties.getSelectedCountyId();
+    $http({
+      method : 'GET',
+      url : '/area/city/' + $scope.countyId
+    }).then(function successCallback(response){
+      $scope.cities = response.data.responseData.cities;
+      $scope.cities.unshift({'city': 'All', 'cityId': 0})
+      $scope.selectedCity = $scope.cities[0]
+    })
+  };
+  $scope.OnChange = function(){
+    sharedProperties.setSelectedCityId($scope.selectedCity.cityId);
+  }
+  
+});
+
+// Controller for verification
 app.controller('viewCtrl', function($scope, $log, sharedProperties){
   $scope.countyId = sharedProperties.getSelectedCountyId();
   $scope.onClick = function(){
